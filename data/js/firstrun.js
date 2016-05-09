38f7344a56b44fd8ce7cf2f2a1b81f7fff99fb96
schalkneethling <sneethling@mozilla.com>
2016-05-09 15:55:16 +0200

    Adds second question, shown to users who selected yes to first question

--- data/js/firstrun.js
+++ data/js/firstrun.js
@@ -1,33 +1,69 @@
 (function() {
     var fxAccountsContainer = document.querySelector('.fxaccounts');
-    var newUserQuery = '<div class="all-aboard-container">' +
-                       '<h2>Are you a new Firefox user?</h2>' +
-                       '<label for="yes">' +
-                       '<input type="radio" name="newuser" value="yes" id="yes" />Yes</label>' +
-                       '<label for="no">' +
-                       '<input type="radio" name="newuser" value="no" id="no" checked />No</label>' +
-                       '<button type="button" class="button">Submit</button></div>';
+    var addonContent;
     var contentContainer;
     var heading;
     var mainContainer;
 
+    // template strings
+    var newUser = '<h2>Are you a new Firefox user?</h2>' +
+                  '<label for="question_one">' +
+                  '<input type="radio" name="isNewUser" value="yes" id="question_one" />Yes</label>' +
+                  '<label for="question_two">' +
+                  '<input type="radio" name="isNewUser" value="no" id="question_two" checked />No</label>' +
+                  '<button type="button" class="button">Submit</button>';
+    var values = '<h2>What matters to you?</h2>' +
+                  '<label for="question_one">' +
+                  '<input type="radio" name="whatMatters" value="values" id="question_one" checked />Values</label>' +
+                  '<label for="question_two">' +
+                  '<input type="radio" name="whatMatters" value="features" id="question_two" />Features</label>' +
+                  '<button type="button" class="button">Submit</button>';
+
+    // shows the default heading and the Fx accounts widget
     function showFxAccountWidget() {
-
         document.querySelector('.all-aboard-container').style.display = 'none';
-
         // show the default heading and the Fx accounts widget
         heading.style.display = 'block';
         mainContainer.style.display = 'block';
     }
 
-    function submitHandler() {
+    // hide the default heading and the Fx accounts widget
+    function hideFxAccountWidget() {
+        heading.style.display = 'none';
+        mainContainer.style.display = 'none';
+    }
+
+    /**
+     * Shows the relevant questions on /firstrun page
+     * @param {string} question - The question id
+     */
+    function showQuestion(question) {
         var button = document.querySelector('.all-aboard-container button');
+        addonContent = contentContainer.querySelector('.all-aboard-container');
+        addonContent.innerHTML = question === 'values' ? values : newUser;
+        contentContainer.focus();
+
+        submitHandler();
+    }
+
+    function submitHandler() {
+        var button = addonContent.querySelector('button');
+        var checkedElem;
         var choice;
+        var name;
 
         button.addEventListener('click', function() {
-            choice = document.querySelector('input[type="radio"]:checked').value;
-            self.port.emit('isNewUser', choice);
-            showFxAccountWidget();
+            checkedElem = addonContent.querySelector('input[type="radio"]:checked');
+            choice = checkedElem.value;
+            name = checkedElem.name;
+
+            self.port.emit(name, choice);
+
+            if (name === 'isNewUser' && choice === 'yes') {
+                showQuestion('values');
+            } else {
+                showFxAccountWidget();
+            }
         });
     }
 
@@ -37,14 +73,12 @@
         heading = document.querySelector('#intro header h2');
         mainContainer = document.querySelector('main');
 
-        // hide the default heading and the Fx accounts widget
-        heading.style.display = 'none';
-        mainContainer.style.display = 'none';
+        hideFxAccountWidget();
 
         // append the question to the main content container
-        contentContainer.insertAdjacentHTML('beforeend', newUserQuery);
-        contentContainer.focus();
+        contentContainer.insertAdjacentHTML('beforeend', '<div class="all-aboard-container"></div>');
 
-        submitHandler();
+        // shows default user question
+        showQuestion();
     }
 })();
