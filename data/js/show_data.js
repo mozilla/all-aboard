d8e0d27a5c8d00cd83ddc383882c17b3d94676d5
FrancescoSTL <francescosapolizzi@gmail.com>
2016-06-15 14:57:55 +0100

    Change tabs to spaces in show_data.js

--- data/js/show_data.js
+++ data/js/show_data.js
@@ -3,13 +3,13 @@
 
     addon.port.on('history', function(history) {
         for(var x = 0, y = history.length; x < y; x++) {
-			document.querySelector('#history' + (x+1)).innerHTML = history[x];
-		}
+            document.querySelector('#history' + (x+1)).innerHTML = history[x];
+        }
     });
 
     addon.port.on('bookmarks', function(bookmarks) {
         for(var x = 0, y = bookmarks.length; x < y; x++) {
             document.querySelector('#bookmarks' + (x+1)).innerHTML = bookmarks[x];
-		}
+        }
     });
 })();
