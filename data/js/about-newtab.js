89b6ebf1f8a7858d81275629140a1f77d2166c61
FrancescoSTL <francescosapolizzi@gmail.com>
2016-06-29 17:25:00 -0700

    Fix automigrate js issue.

--- data/js/about-newtab.js
+++ data/js/about-newtab.js
@@ -2,18 +2,24 @@
 
 var headDiv = document.querySelector('#newtab-search-container');
 var footDiv = document.querySelector('#newtab-margin-bottom');
-// staging for autoimport code
-//var undoMigrate = document.querySelector('#undo-migrate');
 
 function showUserData(headerContent, footerContent) {
     headDiv.insertAdjacentHTML('beforebegin', headerContent);
     footDiv.insertAdjacentHTML('beforebegin', footerContent);
+
+    /* staging for auto migrate
+    var undoMigrate = document.querySelector('#undo-migrate');
+
+    undoMigrate.addEventListener('click', function() {
+	    addon.port.emit('undoMigrate');
+	});*/
 }
 
 // listen for the modify event emitted from the add-on, and only then,
 // start execution of the code.
 self.port.on('modify', function(headerContent, footerContent) {
     showUserData(headerContent, footerContent);
+
 });
 
 // listen for the remove footer event
@@ -23,9 +29,3 @@ self.port.on('removeFooter', function() {
     // hide the footer content
     footer.style.visibility = "hidden";
 });
-
-/* staging for autoimport code
-undoMigrate.addEventListener('click', function() {
-    addon.port.emit('undoMigrate');
-});
-*/
