0234b92ae967ae5858d43ed4095b5581435eac20
FrancescoSTL <francescosapolizzi@gmail.com>
2016-06-30 14:34:29 -0700

    Issue #213 - Fix Smarton/newsletter URL

--- index.js
+++ index.js
@@ -329,10 +329,10 @@ function showSidebar(sidebarProps) {
                         showSearch();
                         break;
                     case 'smarton':
-                        tabs.open('https://www.mozilla.org/teach/smarton/security/');
+                        tabs.open('https://www.mozilla.org/teach/smarton/security/?utm_source=fxonboarding&amp;utm_medium=firefox-browser&amp;utm_campaign=onboardingv1&amp;utm_content=security/');
                         break;
                     case 'newsletter':
-                        tabs.open('https://www.mozilla.org/#newsletter-subscribe');
+                        tabs.open('https://www.mozilla.org/?utm_source=fxonboarding&amp;utm_medium=firefox-browser&amp;utm_campaign=onboardingv1&amp;utm_content=community#newsletter-subscribe');
                         break;
                     case 'privateBrowsing':
                         highLight('privateWindow');
