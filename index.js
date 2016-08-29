3284ae8afd4e9cd77ea782bafba6eeb996dbc445
schalkneethling <sneethling@mozilla.com>
2016-08-29 15:07:52 +0200

    Fix issue#328, TG-157 #closed, Speed up first sidebar interaction

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var timers = require('sdk/timers');
 // All Aboard module imports
 var { intervals } = require('lib/intervals.js');
 var { notificationsManager } = require('lib/notification-manager.js');
+var { scheduler } = require('lib/scheduler.js');
 var { sidebarManager } = require('lib/sidebar-manager.js');
 var { storageManager } = require('lib/storage-manager.js');
 var { toolbarButton } = require('lib/toolbar-button.js');
@@ -35,20 +36,46 @@ exports.main = function() {
     // set's up the addon for dev mode.
     utils.overrideDefaults();
 
+    let lastCTACompleteTime = storageManager.get('lastSidebarCTACompleteTime');
+
+    // the user has not seen the first sidebar, and has not received the first notification but,
+    // the user has completed firstrun.
+    if (typeof storageManager.get('step') === 'undefined'
+        && typeof lastCTACompleteTime !== 'undefined'
+        && storageManager.get('shownNotification') === false) {
+
+        let timeSinceLastCTAInteraction = Date.now() - lastCTACompleteTime;
+
+        // add the add-on ActionButton
+        toolbarButton.addAddOnButton();
+
+        // it has been more than 2 hours since firstrun was completed.
+        // Trigger the delayedNotification which will pop a notification
+        // after a 60 second delay.
+        if (timeSinceLastCTAInteraction > intervals.waitInterval / 12) {
+            scheduler.delayedNotification();
+        } else {
+            // it has been less then two hours between restarts so, simply
+            // reschedule the first sidebar notification for two hours from now.
+            scheduler.startNotificationTimer(12);
+        }
+    }
+
     // if the user has seen at least step 1, and we aren't already to the reward sidebar
     // OR if the user is onboarding and hasn't seen a step yet or they aren't to the 5th step,
-    // add the get the sidebar props for the current step, add the actionButton, and modify about:home
+    // get the sidebar props for the current step, add the actionButton, and modify about:home
     if ((typeof storageManager.get('step') !== 'undefined' && storageManager.get('step') < 5) ||
-        (typeof storageManager.get('isOnBoarding') !== 'undefined' &&
-        (storageManager.get('step') < 5 || typeof storageManager.get('step') === 'undefined'))) {
+        typeof storageManager.get('isOnBoarding') !== 'undefined' && storageManager.get('step') < 5) {
         toolbarButton.addAddOnButton();
-        sidebarManager.sidebarProps = sidebarManager.getSidebarProps();
-        aboutHome.modifyAboutHome(sidebarManager.sidebarProps.track, sidebarManager.sidebarProps.step);
-    // else, if we've reached the reward sidebar, so just add the all-aboard button to the page
+        sidebarManager.setSidebarProps();
+        aboutHome.modifyAboutHome(storageManager.get('sidebarProps'));
+    // else, if we've reached the reward sidebar, just add the all-aboard button to the page
     } else if (storageManager.get('step') >= 5) {
         //add the addon button
         toolbarButton.addAddOnButton();
-        aboutHome.modifyAboutHome('reward');
+        aboutHome.modifyAboutHome({
+            track: 'reward'
+        });
     }
 
     // When Firefox opens, we should check and see if about:home is loaded as the active homepage.
@@ -62,7 +89,8 @@ exports.main = function() {
         console.error('Could not reload snippet content: ', e);
     }
 
-    // catch the anomaly where users aren't getting a notification because their lastSidebarCTACompleteTime isn't set, but they ARE onboarding
+    // catch the anomaly where users aren't getting a notification because their
+    // lastSidebarCTACompleteTime isn't set, but they ARE onboarding
     if(typeof storageManager.get('lastSidebarCTACompleteTime') === 'undefined' &&
         typeof storageManager.get('isOnBoarding') !== 'undefined') {
         // update the lastSidebarCTACompleteTime to 24 hours prior to when this line is hit
@@ -80,9 +108,7 @@ exports.main = function() {
         && typeof storageManager.get('rewardSidebarShown') === 'undefined'))
         && (!storageManager.get('shownNotification')))  {
         // if all of the above is true, wait 60 seconds and then notify
-        timers.setTimeout(function() {
-            notificationsManager.showBadge();
-        }, intervals.oneMinute);
+        scheduler.delayedNotification();
 
     // If 24 hours hasn't yet elapsed and we haven't yet shown the reward sidebar, start a new timer as if
     // the old timer never stopped counting
