11c4e475492294ae879ad75f8cb2ff0e4ad6337c
schalkneethling <sneethling@mozilla.com>
2017-01-11 15:21:50 +0200

    Fix issue #300, l10n initial strings extracted and util

--- data/js/about-newtab.js
+++ data/js/about-newtab.js
@@ -3,28 +3,27 @@
 var headDiv = document.querySelector('#newtab-search-container');
 var footDiv = document.querySelector('#newtab-margin-bottom');
 
-function showUserData(headerContent, footerContent) {
-    // if we have header content, display it
-    if (headerContent) {
-        headDiv.insertAdjacentHTML('beforebegin', headerContent);
+function showUserData(templates, showFooter) {
+    headDiv.insertAdjacentHTML('beforebegin', templates.header);
 
-        var history = document.querySelector('#history');
-        var bookmarks = document.querySelector('#bookmarks');
+    var history = document.querySelector('#history');
+    var bookmarks = document.querySelector('#bookmarks');
 
-        history.addEventListener('click', function() {
-            self.port.emit('intent', 'showAwesomebar');
-        });
+    history.addEventListener('click', function() {
+        self.port.emit('intent', 'showAwesomebar');
+    });
 
-        bookmarks.addEventListener('click', function() {
-            self.port.emit('intent', 'showBookmarks');
-        });
+    bookmarks.addEventListener('click', function() {
+        self.port.emit('intent', 'showBookmarks');
+    });
 
-        // As soon as the script loads and modifys the page, emit a message back to our sidebar that we've loaded the page
-        self.port.emit('pageModified');
-    }
+    // As soon as the script loads and modifys the page, emit a message back to our
+    // sidebar that we've loaded the page
+    self.port.emit('pageModified');
+
     // if we have footer content, display it
-    if (footerContent) {
-        footDiv.insertAdjacentHTML('beforebegin', footerContent);
+    if (showFooter) {
+        footDiv.insertAdjacentHTML('beforebegin', templates.footer);
 
         var undoMigrate = document.querySelector('#undo-migrate');
 
@@ -36,11 +35,11 @@ function showUserData(headerContent, footerContent) {
 
 // listen for the modify event emitted from the add-on, and only then,
 // start execution of the code.
-self.port.on('modify', function(headerContent, footerContent) {
+self.port.on('modify', function(templates, showFooter) {
     // try to grab the stuff we're going to insert into the page
     var autoImportHeader = document.querySelector('.allaboard-auto-import');
     // if it isn't inserted already, call the function to insert it
     if (autoImportHeader === null) {
-        showUserData(headerContent, footerContent);
+        showUserData(templates, showFooter);
     }
 });
