32a7d28a3b5de66b9075369bc7700bd26a135ea6
schalkneethling <sneethling@mozilla.com>
2017-02-08 10:38:22 +0200

    Fix issue #514, move look for another.. to below stars

--- data/js/utility/content2.js
+++ data/js/utility/content2.js
@@ -2,11 +2,22 @@
     // retrieve button from the page
     var button = document.querySelector('button');
 
+    /**
+     * Shows the tips copy below the progression bar when
+     * main CTA is completed.
+     */
+    function showTipsCopy() {
+        let tipsCopy = document.getElementById('tips-copy');
+        tipsCopy.classList.remove('hide');
+        tipsCopy.setAttribute('aria-hidden', false);
+    }
+
     // add event listeners for a button click on the button
     button.addEventListener('click', function() {
         // notify addon that we've clicked the button
         addon.port.emit('intent', 'privateBrowsing');
         // main cta action complete, notify add-on
         addon.port.emit('cta_complete');
+        showTipsCopy();
     });
 })();
--- data/js/utility/content3.js
+++ data/js/utility/content3.js
@@ -1,12 +1,17 @@
 (function() {
     // retrieve our apple store button
-    var appleStore = document.querySelector('#appleStore');
+    let appleStore = document.querySelector('#appleStore');
+    // get the tips copy element
+    let tipsCopy = document.getElementById('tips-copy');
     // retrieve our play store button
-    var playStore = document.querySelector('#playStore');
+    let playStore = document.querySelector('#playStore');
 
     // by opening this sidebar hte main cta action is
     // complete, notify add-on
     addon.port.emit('cta_complete');
+    // show info copy
+    tipsCopy.classList.remove('hide');
+    tipsCopy.setAttribute('aria-hidden', false);
 
     // add click listener on default theme text option
     appleStore.addEventListener('click', function(event) {
